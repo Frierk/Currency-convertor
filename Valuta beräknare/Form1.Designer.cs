4ce9c77939a73f04a620e2de398d5b81b1d26478
Frierk <Erik.friman@skola.sala.se>
2025-09-08 15:06:14 +0200

    Gjort klart vänster till höger knappen för custom valutan.

--- Valuta beräknare/Form1.Designer.cs
+++ Valuta beräknare/Form1.Designer.cs
@@ -35,13 +35,26 @@
             textBox1 = new TextBox();
             textBox2 = new TextBox();
             label1 = new Label();
-            button1 = new Button();
+            buttonCl = new Button();
+            buttonVtHSp = new Button();
+            buttonHtVSp = new Button();
+            ButtonClSp = new Button();
+            textBox3 = new TextBox();
+            textBox4 = new TextBox();
+            textBox5 = new TextBox();
+            textBox6 = new TextBox();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            textBox7 = new TextBox();
+            label6 = new Label();
             SuspendLayout();
             // 
             // comboBox1
             // 
             comboBox1.FormattingEnabled = true;
-            comboBox1.Items.AddRange(new object[] { "SEK", "EUR", "USD", "GBP" });
+            comboBox1.Items.AddRange(new object[] { "SEK", "EUR", "USD", "GBP", "Min valuta" });
             comboBox1.Location = new Point(121, 152);
             comboBox1.Name = "comboBox1";
             comboBox1.Size = new Size(96, 33);
@@ -50,7 +63,7 @@
             // comboBox2
             // 
             comboBox2.FormattingEnabled = true;
-            comboBox2.Items.AddRange(new object[] { "SEK", "EUR", "USD", "GBP" });
+            comboBox2.Items.AddRange(new object[] { "SEK", "EUR", "USD", "GBP", "Min valuta" });
             comboBox2.Location = new Point(507, 152);
             comboBox2.Name = "comboBox2";
             comboBox2.Size = new Size(96, 33);
@@ -100,22 +113,150 @@
             label1.TabIndex = 6;
             label1.Text = "Valutakonverterare";
             // 
-            // button1
+            // buttonCl
             // 
-            button1.Location = new Point(348, 252);
-            button1.Name = "button1";
-            button1.Size = new Size(112, 34);
-            button1.TabIndex = 7;
-            button1.Text = "Clear";
-            button1.UseVisualStyleBackColor = true;
-            button1.Click += button1_Click;
+            buttonCl.Location = new Point(348, 252);
+            buttonCl.Name = "buttonCl";
+            buttonCl.Size = new Size(112, 34);
+            buttonCl.TabIndex = 7;
+            buttonCl.Text = "Clear";
+            buttonCl.UseVisualStyleBackColor = true;
+            buttonCl.Click += buttonCl_Click;
+            // 
+            // buttonVtHSp
+            // 
+            buttonVtHSp.Location = new Point(348, 418);
+            buttonVtHSp.Name = "buttonVtHSp";
+            buttonVtHSp.Size = new Size(112, 34);
+            buttonVtHSp.TabIndex = 8;
+            buttonVtHSp.Text = "--->";
+            buttonVtHSp.UseVisualStyleBackColor = true;
+            buttonVtHSp.Click += buttonVtHSp_Click;
+            // 
+            // buttonHtVSp
+            // 
+            buttonHtVSp.Location = new Point(348, 458);
+            buttonHtVSp.Name = "buttonHtVSp";
+            buttonHtVSp.Size = new Size(112, 34);
+            buttonHtVSp.TabIndex = 9;
+            buttonHtVSp.Text = "<---";
+            buttonHtVSp.UseVisualStyleBackColor = true;
+            buttonHtVSp.Click += buttonHtVSp_Click;
+            // 
+            // ButtonClSp
+            // 
+            ButtonClSp.Location = new Point(348, 498);
+            ButtonClSp.Name = "ButtonClSp";
+            ButtonClSp.Size = new Size(112, 34);
+            ButtonClSp.TabIndex = 10;
+            ButtonClSp.Text = "Clear";
+            ButtonClSp.UseVisualStyleBackColor = true;
+            ButtonClSp.Click += ButtonClSp_Click;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(121, 441);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(182, 31);
+            textBox3.TabIndex = 11;
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(491, 441);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(182, 31);
+            textBox4.TabIndex = 12;
+            // 
+            // textBox5
+            // 
+            textBox5.Location = new Point(491, 389);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(112, 31);
+            textBox5.TabIndex = 13;
+            // 
+            // textBox6
+            // 
+            textBox6.Location = new Point(191, 389);
+            textBox6.Name = "textBox6";
+            textBox6.ReadOnly = true;
+            textBox6.Size = new Size(112, 31);
+            textBox6.TabIndex = 14;
+            textBox6.Text = "1";
+            textBox6.TextAlign = HorizontalAlignment.Center;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(393, 389);
+            label2.Name = "label2";
+            label2.Size = new Size(24, 25);
+            label2.TabIndex = 15;
+            label2.Text = "=";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 14F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label3.Location = new Point(305, 322);
+            label3.Name = "label3";
+            label3.Size = new Size(194, 38);
+            label3.TabIndex = 16;
+            label3.Text = "Custom Valuta";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 10F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label4.Location = new Point(781, 157);
+            label4.Name = "label4";
+            label4.Size = new Size(104, 28);
+            label4.TabIndex = 18;
+            label4.Text = "Min valuta";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(891, 160);
+            label5.Name = "label5";
+            label5.Size = new Size(24, 25);
+            label5.TabIndex = 19;
+            label5.Text = "=";
+            // 
+            // textBox7
+            // 
+            textBox7.Location = new Point(921, 157);
+            textBox7.Name = "textBox7";
+            textBox7.Size = new Size(90, 31);
+            textBox7.TabIndex = 20;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(1017, 160);
+            label6.Name = "label6";
+            label6.Size = new Size(38, 25);
+            label6.TabIndex = 21;
+            label6.Text = "sek";
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
-            Controls.Add(button1);
+            ClientSize = new Size(1101, 583);
+            Controls.Add(label6);
+            Controls.Add(textBox7);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(textBox6);
+            Controls.Add(textBox5);
+            Controls.Add(textBox4);
+            Controls.Add(textBox3);
+            Controls.Add(ButtonClSp);
+            Controls.Add(buttonHtVSp);
+            Controls.Add(buttonVtHSp);
+            Controls.Add(buttonCl);
             Controls.Add(label1);
             Controls.Add(textBox2);
             Controls.Add(textBox1);
@@ -138,6 +279,19 @@
         private TextBox textBox1;
         private TextBox textBox2;
         private Label label1;
-        private Button button1;
+        private Button buttonCl;
+        private Button buttonVtHSp;
+        private Button buttonHtVSp;
+        private Button ButtonClSp;
+        private TextBox textBox3;
+        private TextBox textBox4;
+        private TextBox textBox5;
+        private TextBox textBox6;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private TextBox textBox7;
+        private Label label6;
     }
 }
--- Valuta beräknare/Form1.cs
+++ Valuta beräknare/Form1.cs
@@ -7,6 +7,7 @@ namespace Valuta_beräknare
             InitializeComponent();
         }
 
+        //Standard
         private void buttonVtH_Click(object sender, EventArgs e)
         {
             if (comboBox1.SelectedIndex == 0)
@@ -27,6 +28,10 @@ namespace Valuta_beräknare
                 {
                     textBox2.Text = (Convert.ToDouble(textBox1.Text) * 0.078).ToString();
                 }
+                if (comboBox2.SelectedIndex == 4)
+                {
+                    textBox2.Text = (Convert.ToDouble(textBox1.Text) / Convert.ToDouble(textBox7.Text)).ToString();
+                }
             }
             if (comboBox1.SelectedIndex == 1)
             {
@@ -45,6 +50,11 @@ namespace Valuta_beräknare
                 if (comboBox2.SelectedIndex == 3)
                 {
                     textBox2.Text = (Convert.ToDouble(textBox1.Text) * 0.87).ToString();
+                }
+                if (comboBox2.SelectedIndex == 4)
+                {
+                    double sek = 1 * Convert.ToDouble(textBox7.Text);
+                    textBox2.Text = (Convert.ToDouble(textBox1.Text) * (sek * 11.08)).ToString();
                 }
             }
             if (comboBox1.SelectedIndex == 2)
@@ -65,6 +75,11 @@ namespace Valuta_beräknare
                 {
                     textBox2.Text = (Convert.ToDouble(textBox1.Text) * 0.74).ToString();
                 }
+                if (comboBox2.SelectedIndex == 4)
+                {
+                    double sek = 1 * Convert.ToDouble(textBox7.Text);
+                    textBox2.Text = (Convert.ToDouble(textBox1.Text) * (sek * 9.44)).ToString();
+                }
             }
             if (comboBox1.SelectedIndex == 3)
             {
@@ -84,6 +99,37 @@ namespace Valuta_beräknare
                 {
                     textBox2.Text = textBox1.Text;
                 }
+                if (comboBox2.SelectedIndex == 4)
+                {
+                    double sek = 1 * Convert.ToDouble(textBox7.Text);
+                    textBox2.Text = (Convert.ToDouble(textBox1.Text) * (sek * 12.76)).ToString();
+                }
+            }
+            if (comboBox1.SelectedIndex == 4)
+            {
+                if (comboBox2.SelectedIndex == 0)
+                {
+                   textBox2.Text = (Convert.ToDouble(textBox1.Text) * Convert.ToDouble(textBox7.Text)).ToString();
+                }
+                if (comboBox2.SelectedIndex == 1)
+                {
+                    double sek = Convert.ToDouble(textBox1.Text) * Convert.ToDouble(textBox7.Text);
+                    textBox2.Text = (sek * 0.09).ToString();
+                }
+                if (comboBox2.SelectedIndex == 2)
+                {
+                    double sek = Convert.ToDouble(textBox1.Text) * Convert.ToDouble(textBox7.Text);
+                    textBox2.Text = (sek * 0.11).ToString();
+                }
+                if (comboBox2.SelectedIndex == 3)
+                {
+                    double sek = Convert.ToDouble(textBox1.Text) * Convert.ToDouble(textBox7.Text);
+                    textBox2.Text = (sek * 0.078).ToString();
+                }
+                if (comboBox2.SelectedIndex == 4)
+                {
+                    textBox2.Text = textBox1.Text;
+                }
             }
         }
         private void buttonHtV_Click(object sender, EventArgs e)
@@ -96,22 +142,22 @@ namespace Valuta_beräknare
                 }
                 if (comboBox1.SelectedIndex == 1)
                 {
-                    textBox1.Text = (Convert.ToDouble(textBox2.Text) * 0.09).ToString();
+                    textBox1.Text = (Convert.ToDouble(textBox2.Text) / 11.08).ToString();
                 }
                 if (comboBox1.SelectedIndex == 2)
                 {
-                    textBox1.Text = (Convert.ToDouble(textBox2.Text) * 0.11).ToString();
+                    textBox1.Text = (Convert.ToDouble(textBox2.Text) / 9.44).ToString();
                 }
                 if (comboBox1.SelectedIndex == 3)
                 {
-                    textBox1.Text = (Convert.ToDouble(textBox2.Text) * 0.078).ToString();
+                    textBox1.Text = (Convert.ToDouble(textBox2.Text) * 12.76).ToString();
                 }
             }
             if (comboBox2.SelectedIndex == 1)
             {
                 if (comboBox1.SelectedIndex == 0)
                 {
-                    textBox1.Text = (Convert.ToDouble(textBox2.Text) * 11.08).ToString();
+                    textBox1.Text = (Convert.ToDouble(textBox2.Text) / 0.09).ToString();
                 }
                 if (comboBox1.SelectedIndex == 1)
                 {
@@ -119,22 +165,22 @@ namespace Valuta_beräknare
                 }
                 if (comboBox1.SelectedIndex == 2)
                 {
-                    textBox1.Text = (Convert.ToDouble(textBox2.Text) * 1.17).ToString();
+                    textBox1.Text = (Convert.ToDouble(textBox2.Text) / 0.85).ToString();
                 }
                 if (comboBox1.SelectedIndex == 3)
                 {
-                    textBox1.Text = (Convert.ToDouble(textBox2.Text) * 0.87).ToString();
+                    textBox1.Text = (Convert.ToDouble(textBox2.Text) / 1.15).ToString();
                 }
             }
             if (comboBox2.SelectedIndex == 2)
             {
                 if (comboBox1.SelectedIndex == 0)
                 {
-                    textBox1.Text = (Convert.ToDouble(textBox2.Text) * 9.44).ToString();
+                    textBox1.Text = (Convert.ToDouble(textBox2.Text) / 0.11).ToString();
                 }
                 if (comboBox1.SelectedIndex == 1)
                 {
-                    textBox1.Text = (Convert.ToDouble(textBox2.Text) * 0.85).ToString();
+                    textBox1.Text = (Convert.ToDouble(textBox2.Text) / 1.17).ToString();
                 }
                 if (comboBox1.SelectedIndex == 2)
                 {
@@ -142,34 +188,55 @@ namespace Valuta_beräknare
                 }
                 if (comboBox1.SelectedIndex == 3)
                 {
-                    textBox1.Text = (Convert.ToDouble(textBox2.Text) * 0.74).ToString();
+                    textBox1.Text = (Convert.ToDouble(textBox2.Text) / 1.35).ToString();
                 }
             }
             if (comboBox2.SelectedIndex == 3)
             {
                 if (comboBox1.SelectedIndex == 0)
                 {
-                    textBox1.Text = (Convert.ToDouble(textBox2.Text) * 12.76).ToString();
+                    textBox1.Text = (Convert.ToDouble(textBox2.Text) / 0.078).ToString();
                 }
                 if (comboBox1.SelectedIndex == 1)
                 {
-                    textBox1.Text = (Convert.ToDouble(textBox2.Text) * 1.15).ToString();
+                    textBox1.Text = (Convert.ToDouble(textBox2.Text) / 0.87).ToString();
                 }
                 if (comboBox1.SelectedIndex == 2)
                 {
-                    textBox1.Text = (Convert.ToDouble(textBox2.Text) * 1.35).ToString();
+                    textBox1.Text = (Convert.ToDouble(textBox2.Text) / 0.74).ToString();
                 }
                 if (comboBox1.SelectedIndex == 3)
                 {
                     textBox1.Text = textBox2.Text;
                 }
             }
+            if (comboBox2.SelectedIndex == 4)
+            {
+                if (comboBox1.SelectedIndex == 0)
+                {
+                    textBox2.Text = (Convert.ToDouble(textBox1.Text) * Convert.ToDouble(textBox7.Text)).ToString();
+                }
+            }
         }
-
-        private void button1_Click(object sender, EventArgs e)
+        private void buttonCl_Click(object sender, EventArgs e)
         {
             textBox1.Text = "";
             textBox2.Text = "";
+        }
+
+        // Custom
+        private void buttonVtHSp_Click(object sender, EventArgs e)
+        {
+            textBox4.Text = (Convert.ToDouble(textBox3.Text) * Convert.ToDouble(textBox5.Text)).ToString();
+        }
+        private void buttonHtVSp_Click(object sender, EventArgs e)
+        {
+            textBox3.Text = (Convert.ToDouble(textBox4.Text) / Convert.ToDouble(textBox5.Text)).ToString();
+        }
+        private void ButtonClSp_Click(object sender, EventArgs e)
+        {
+            textBox3.Text = "";
+            textBox4.Text = "";
         }
     }
 }
